8160c3d8cf740223921fb1d9b6a898adb1a7feac
Alperen Özkan <alperenweb3@gmail.com>
2023-04-28 20:45:57 +0200

    React Router Dom installed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
